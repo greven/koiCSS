d01cbbd264914d939c04c05af1100ca553014731
Nuno Freire <greven.pt@gmail.com>
2022-05-02 21:33:39 +0100

    🦄

--- dev/postcss.config.js
+++ dev/postcss.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   plugins: [
     require('autoprefixer'),
-    // require('postcss-preset-env')({ stage: 2 }),
-    // require('cssnano')({ preset: 'default' }),
-    // require('@fullhuman/postcss-purgecss')({
-    // content: ['./**/*.html'],
-    // }),
+    require('postcss-preset-env')({ stage: 2 }),
+    require('cssnano')({ preset: 'default' }),
+    require('@fullhuman/postcss-purgecss')({
+      content: ['./**/*.html'],
+    }),
   ],
 }
